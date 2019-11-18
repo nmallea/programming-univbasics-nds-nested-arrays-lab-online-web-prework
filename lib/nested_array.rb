77cfb2b8320e406ea764f5a53188a971ecf592d9
Nikane Mallea <nikane@mac.com>
2019-11-18 15:04:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  matrix_lookup(assembled_matrix.ENFIELD_TENNIS_ACADEMY[1][1])
+  return matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
